c09ad54a215d17594b4116133e057ef0ba18bf9c
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-21 16:59:00 -0400

    Initial commit for pdas project

---
+++ pdas/src/App.js
@@ -0,0 +1,17 @@
+import ProfileCard from "./ProfileCard";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <div>Personal Digital Assistant</div>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+}
+
+
+export default App;
