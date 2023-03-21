70ec28dc395dbdc1669117d816815e22e9a6f407
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-20 20:46:18 -0400

    Writing expressions inside jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,11 +10,7 @@ const root = ReactDOM.createRoot(el);
 // 4) Create a component
 
 function App() {
-  let message = "Bye there!!";
-  if (Math.random() > 0.5) {
-    message = "Hello there!!";
-  }
-  return <h1>{message}</h1>;
+  return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 // 5) Show the component on the screen
 root.render(<App />);
