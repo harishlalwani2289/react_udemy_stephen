c4c3167a9c80252b4b30f31a512316690ac8aedd
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-22 08:37:44 -0400

    PAssing the image from App to Profile Card

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
   // This line is also not required
   //   const { title, handle } = props;
   return (
     <div>
+      <img src={image} alt="pda logo" />
       PDA name: {title} with twitter handle {handle}
     </div>
   );
