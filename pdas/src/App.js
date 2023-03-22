75a532811ce5cb2b9541f7e8bfc7d4c1ac00fd21
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-22 08:30:45 -0400

    Adding images. different between images greater than 10kb and less than 10kb

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,5 +1,11 @@
 import ProfileCard from "./ProfileCard";
 import React from "react";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
