5388058e173fcdfe52bf2bca4d525ec02863ac69
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-22 17:11:33 -0400

    Added a useState use for increasing count

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   const handleClick = () => {
     console.log("Button was clicked!!");
+    setCount(count + 1);
   };
+
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>Animals to show {count}</div>
     </div>
   );
 }
