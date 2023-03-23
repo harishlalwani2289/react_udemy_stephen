168746e73423924606393acc71fa366a23e6057d
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-22 20:57:11 -0400

    Complete animal app with styling

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,5 +1,42 @@
-function AnimalShow() {
-  return <div>Cow</div>;
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+import { useState } from "react";
+import "./AnimalShow.css";
+
+const svgMAp = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+  return (
+    <div onClick={handleClick} className="animal-show">
+      <img alt="animal" src={svgMAp[type]} className="animal"></img>
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{
+          width: 10 + 10 * clicks + "px",
+          height: 10 + 10 * clicks + "px",
+        }}
+      ></img>
+    </div>
+  );
 }
 
 export default AnimalShow;
--- animals/src/App.js
+++ animals/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import "./App.css";
 
 function getRandomAnimal() {
   const animals = ["bird", "cat", "dog", "cow", "gator", "horse"];
@@ -13,10 +15,14 @@ function App() {
     setAnimals([...animals, getRandomAnimal()]);
   };
 
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index}></AnimalShow>;
+  });
+
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>{animals}</div>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
