c0fbd08c655a43a9ab847a7aa522390dc9022b93
Harish Lalwani <harishlalwani2289@gmail.com>
2023-03-25 19:04:30 -0400

    BookCreate component done

--- books/src/App.js
+++ books/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Need to add book with tile  :", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook}></BookCreate>
+    </div>
+  );
 }
 
 export default App;
