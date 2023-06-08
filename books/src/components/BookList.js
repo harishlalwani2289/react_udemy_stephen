348da0de0b8f1f6a3c732b47f02233b789d4ccad
Harish Lalwani <harishlalwani2289@gmail.com>
2023-06-08 17:02:04 -0400

    Context in Action : Intitial understanding code

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,12 +1,25 @@
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
+import { useContext } from "react";
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
     return (
-      <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
+      <BookShow
+        key={book.id}
+        book={book}
+        onDelete={onDelete}
+        onEdit={onEdit}
+      />
     );
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {value}
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
